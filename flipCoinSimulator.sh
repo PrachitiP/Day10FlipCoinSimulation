47ba13075e8b10bf4c68de2ded067023d1d30e6f
PrachitiP <patilprachiti35@gmail.com>
2020-08-29 13:34:54 +0530

    Flip Coin Multiple Times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,3 +9,28 @@ then
 else
 	echo "Tail"
 fi
+
+# UseCase 2 => Flip Coin multiple times
+count=0;
+hcount=0
+tcount=0;
+while [ $count -ne 21 ]
+do
+	var=$((RANDOM%2))
+	#echo $var
+
+	if [[ $var -eq 1 ]]
+	then
+		echo "Head"
+		((hcount++))
+		
+	else
+
+		echo "Tail"
+		((tcount++))
+
+	fi
+	((count++))
+done
+echo "Head wins $hcount times"
+echo "Tail wins $tcount times"
